69acba5343d353c326fefd0a4648e5399dcc10ae
Charles Mickael <mickael.charles5@etu.univ-lorraine.fr>
2015-02-18 16:19:43 +0100

    ajout du tampon de profondeur pour un rendu beaucoup plus realiste (on ne dessine plus les parties face a la lumiere mais cachees)

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ vector<vector<float> > sommets ;
 vector<vector<float> > textures ;
 vector<vector<float> > normaux  ;
 vector<vector<vector<int> > > facets  ;
+int ** zbuffer = NULL ; int maxi = 0 ;
+int taille = 0 ;
 
 void readFile(string path) {
 	ifstream fichier(path.c_str()) ;
@@ -51,8 +53,10 @@ void readFile(string path) {
 	} else cout << "OUPS" << endl ;
 }
 
-void line(TGAImage &image, int x1, int y1, int x2, int y2, TGAColor color){
+
+void line(TGAImage &image, int x1, int y1, int z1, int x2, int y2, int z2, TGAColor color){
 	float t, y ;
+	int z ;
 	bool pentu = false ;
 	if((pentu = abs(y2-y1)>abs(x2-x1))){
 		swap(x1,y1) ;
@@ -61,52 +65,66 @@ void line(TGAImage &image, int x1, int y1, int x2, int y2, TGAColor color){
 	if(x2<x1) {
 		swap(x1,x2) ;
 		swap(y1,y2) ;
+		swap(z1,z2) ;
 	}
+
 	for(int x=x1; x<=x2; x++){
-		t = (x2==x1) ? 1. : (x-x1)/(float)(x2-x1) ;
-		y = (1.-t)*y1 + t*y2 ;
-		y = (y-int(y) >= 0.5) ? ceil(y) : floor(y) ;
-		
-		if(pentu)
-			image.set(y,x,color) ;
-		else
-			image.set(x,y,color) ;
+		t = (x2==x1) ? 1 : (x-x1)/(float)(x2-x1) ;
+		y = (1-t)*y1 + t*y2 ;
+		z = (1-t)*z1 + t*z2 ;
+
+		if(pentu){
+			if(x>0 && y>0 && x<taille && y<taille && zbuffer[(int)y][x] < z){
+				zbuffer[(int)y][x] = z ;
+				if(maxi < z) maxi = z ;
+				image.set(y,x,color) ;
+			}
+		}else{
+			if(x>0 && y>0 && x<taille && y<taille && zbuffer[x][(int)y] < z){
+				zbuffer[x][(int)y] = z ;
+				if(maxi < z) maxi = z ;
+				image.set(x,y,color) ;
+			}
+		}
 	}
 }
 
-void triangle(TGAImage &image, int x1, int y1, int x2, int y2, int x3, int y3, TGAColor color){
-	float y_h, y_b, t1, t2 ;
+void triangle(TGAImage &image, int x1, int y1, int z1, int x2, int y2, int z2, int x3, int y3, int z3, TGAColor color){
+	float y_h, y_b,z_h, z_b,  t1, t2 ;
 	if (x2<x1){
 		swap(x2,x1) ;
 		swap(y2,y1) ;
+		swap(z2,z1) ;
 	}
 	if (x3<x1){
 		swap(x3,x1) ;
 		swap(y3,y1) ;
+		swap(z3,z1) ;
 	}
 	if (x3<x2){
 		swap(x3,x2) ;
 		swap(y3,y2) ;
+		swap(z3,z2) ;
 	}
-	for(int x = x1; x<=x2; x++){
-		t1 = (x2==x1) ? 1. : (x-x1)/(float)(x2-x1) ;
-		t2 = (x3==x1) ? 1. : (x-x1)/(float)(x3-x1) ;
+	for(int x = x1; x<x2; x++){
+		t1 = (x2==x1) ? 1 : (x-x1)/(float)(x2-x1) ;
+		t2 = (x3==x1) ? 1 : (x-x1)/(float)(x3-x1) ;
 		y_h = (1.-t1)*y1 + t1*y2 ;
 		y_b = (1.-t2)*y1 + t2*y3 ;
-		y_h = (y_h-int(y_h) >= 0.5) ? ceil(y_h) : floor(y_h) ;
-		y_b = (y_b-int(y_b) >= 0.5) ? ceil(y_b) : floor(y_b) ;
-		
-		line(image, x,y_h,x,y_b, color) ;
+		z_h = (1.-t1)*z1 + t1*z2 ;
+		z_b = (1.-t2)*z1 + t2*z3 ;
+
+		line(image, x,y_h,z_h, x,y_b,z_b, color) ;
 	}
 	for(int x = x2; x<=x3; x++){
-		t1 = (x3==x2) ? 1. : (x-x2)/(float)(x3-x2) ;
-		t2 = (x3==x1) ? 1. : (x-x1)/(float)(x3-x1) ;
+		t1 = (x3==x2) ? 1 : (x-x2)/(float)(x3-x2) ;
+		t2 = (x3==x1) ? 1 : (x-x1)/(float)(x3-x1) ;
 		y_h = (1.-t1)*y2 + t1*y3 ;
 		y_b = (1.-t2)*y1 + t2*y3 ;
-		y_h = (y_h-int(y_h) >= 0.5) ? ceil(y_h) : floor(y_h) ;
-		y_b = (y_b-int(y_b) >= 0.5) ? ceil(y_b) : floor(y_b) ;
-		
-		line(image, x,y_h,x,y_b, color) ;
+		z_h = (1.-t1)*z2 + t1*z3 ;
+		z_b = (1.-t2)*z1 + t2*z3 ;
+
+		line(image, x,y_h,z_h, x,y_b,z_b, color) ;
 	}
 }
 
@@ -129,7 +147,13 @@ struct Vec3f {
 
 int main(int argc, char** argv){
 	readFile("obj/african_head.obj") ;
-	int w = 1000 ;
+	const int w = 1000 ;
+	zbuffer = new int*[w] ;
+	for(int i = 0; i<w; i++){
+		zbuffer[i] = new int[w] ;
+		for(int j =0; j<w; j++) zbuffer[i][j] = 0 ;
+	}
+	taille = w-1 ;
 	TGAImage im = TGAImage(w,w,1) ;
 	TGAColor blanc = TGAColor(255, 255, 255, 3) ;
 	Vec3f lum(0,0,1) ;
@@ -153,9 +177,9 @@ int main(int argc, char** argv){
 		scalaire = n*lum ;
 		// Affichage
 		if(scalaire > 0)
-			triangle(im, x1,y1,x2,y2,x3,y3, TGAColor(scalaire*255, 1)) ;
+			triangle(im, x1,y1,z1,x2,y2,z2,x3,y3,z3, TGAColor(scalaire*255, 1)) ;
 	}
 	im.flip_vertically() ;
-	im.write_tga_file("ombrage_plat.tga") ;
+	im.write_tga_file("zbuffer.tga") ;
 	return 0 ;
 }
