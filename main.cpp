22ee300237f60101f885fac2c360a16929b15501
Charles Mickael <mickael.charles5@etu.univ-lorraine.fr>
2015-02-18 19:30:05 +0100

    divers correction de matrice et d'affichage

--- main.cpp
+++ main.cpp
@@ -15,11 +15,11 @@ std::vector<Vec3f > textures;
 std::vector<Vec3f > normaux;
 std::vector<std::vector<Vec3i> > facets;
 Vec3f lumiere = Vec3f(1,1,1) ;
-Vec3f cam = Vec3f(0,0,1) ;
+Vec3f cam = Vec3f(-1,1,4) ;
 Vec3f center = Vec3f(0,0,0) ;
 Vec3f u = Vec3f(0,1,0) ;
 Matrix view, projection, changementBase, M, M_inv;
-int ** zbuffer = NULL ; int maxi = 0 ;
+float ** zbuffer = NULL ; int maxi = 0 ;
 const int w = 1000 ;
 
 // PROTOTYPE
@@ -65,10 +65,10 @@ void viewport(int x, int y, int w, int h) {
 	view = Matrix::identity();
 	view[0][3] = x+w/2.f;
 	view[1][3] = y+h/2.f;
-	view[2][3] = 1.f;
+	view[2][3] = 0;
 	view[0][0] = w/2.f;
 	view[1][1] = h/2.f;
-	view[2][2] = 0;
+	view[2][2] = (w+h)/2.f;
 }
 
 void project(float coeff){
@@ -90,53 +90,8 @@ void lookat(Vec3f eye, Vec3f up, Vec3f centre) {
 	}
 }
 
-void transform() {
-	for(int i = 0; i<(int)sommets.size(); i++)
-		sommets[i] = proj<3>(M*embed<4>(sommets[i])) ;
-	for(int i = 0; i<(int)normaux.size(); i++)
-		normaux[i] = proj<3>(M_inv*embed<4>(normaux[i], 0.f)).normalize() ;
-}
-
-void createImage(){
-	TGAImage im = TGAImage(w,w,1) ;
-	for(int i = 0; i<w; i++){
-		zbuffer[i] = new int[w] ;
-		for(int j =0; j<w; j++) zbuffer[i][j] = -1000000 ;
-	}
-	float x1,x2,x3,y1,y2,y3,z1,z2,z3, scalaire1,scalaire2,scalaire3,moyenne ;
-	for(int i = 0; i<facets.size();i++){
-		// Zoom + Translation
-		x1 = sommets[facets[i][0][0]-1][0] ;
-		y1 = sommets[facets[i][0][0]-1][1];
-		z1 = sommets[facets[i][0][0]-1][2];
-		x2 = sommets[facets[i][1][0]-1][0];
-		y2 = sommets[facets[i][1][0]-1][1];
-		z2 = sommets[facets[i][1][0]-1][2];
-		x3 = sommets[facets[i][2][0]-1][0];
-		y3 = sommets[facets[i][2][0]-1][1];
-		z3 = sommets[facets[i][2][0]-1][2];
-		Vec3f n1(normaux[facets[i][0][2]-1]) ;
-		Vec3f n2(normaux[facets[i][1][2]-1]) ;
-		Vec3f n3(normaux[facets[i][2][2]-1]) ;
-		n1 = n1.normalize() ;
-		n2 = n2.normalize() ;
-		n3 = n3.normalize() ;
-		lumiere = lumiere.normalize() ;
-		//	Coloration
-		scalaire1 = max(lumiere * n1, 0.f);
-		scalaire2 = max(lumiere * n2, 0.f);
-		scalaire3 = max(lumiere * n3, 0.f);
-		moyenne = (scalaire1+scalaire2+scalaire3)/3 ;
-		if(moyenne > 0)
-			 triangle(im, x1,y1,z1, TGAColor(scalaire1*255,1),x2,y2,z2, TGAColor(scalaire2*255,1),x3,y3,z3, TGAColor(scalaire3*255,1)) ;
-	}
-	im.flip_vertically() ;
-	im.write_tga_file("ombrage_lisse.tga") ;
-}
-
 void line(TGAImage &image, int x1, int y1, int z1, TGAColor color1, int x2, int y2, int z2, TGAColor color2){
-	float t, y ;
-	int z ;
+	float t, y, z ;
 	TGAColor color(0,1) ;
 	bool pentu = false ;
 	if((pentu = abs(y2-y1)>abs(x2-x1))){
@@ -224,15 +179,60 @@ void triangle(TGAImage &image, int x1, int y1, int z1, TGAColor color1, int x2, 
 	}
 }
 
+void transform() {
+	Vec4f tmp ;
+	for(int i = 0; i<(int)sommets.size(); i++){
+		tmp = M*embed<4>(sommets[i]) ;
+		sommets[i] = proj<3>(tmp)/tmp[3] ;
+	}
+	for(int i = 0; i<(int)normaux.size(); i++)
+		normaux[i] = proj<3>(M_inv*embed<4>(normaux[i], 0.f)).normalize() ;
+}
+
+void createImage(){
+	TGAImage im = TGAImage(w,w,1) ;
+	for(int i = 0; i<w; i++){
+		zbuffer[i] = new float[w] ;
+		for(int j =0; j<w; j++) zbuffer[i][j] = -1000000 ;
+	}
+	float x1,x2,x3,y1,y2,y3,z1,z2,z3, scalaire1,scalaire2,scalaire3,affich ;
+	for(int i = 0; i<facets.size();i++){
+		// Zoom + Translation
+		x1 = sommets[facets[i][0][0]-1][0] ;
+		y1 = sommets[facets[i][0][0]-1][1];
+		z1 = sommets[facets[i][0][0]-1][2];
+		x2 = sommets[facets[i][1][0]-1][0];
+		y2 = sommets[facets[i][1][0]-1][1];
+		z2 = sommets[facets[i][1][0]-1][2];
+		x3 = sommets[facets[i][2][0]-1][0];
+		y3 = sommets[facets[i][2][0]-1][1];
+		z3 = sommets[facets[i][2][0]-1][2];
+		Vec3f n1(normaux[facets[i][0][2]-1]) ;
+		Vec3f n2(normaux[facets[i][1][2]-1]) ;
+		Vec3f n3(normaux[facets[i][2][2]-1]) ;
+		n1 = n1.normalize() ;
+		n2 = n2.normalize() ;
+		n3 = n3.normalize() ;
+		lumiere = lumiere.normalize() ;
+		//	Coloration
+		scalaire1 = max(lumiere * n1, 0.f);
+		scalaire2 = max(lumiere * n2, 0.f);
+		scalaire3 = max(lumiere * n3, 0.f);
+		triangle(im, x1,y1,z1, TGAColor(scalaire1*255,1),x2,y2,z2, TGAColor(scalaire2*255,1),x3,y3,z3, TGAColor(scalaire3*255,1)) ;
+	}
+	im.flip_vertically() ;
+	im.write_tga_file("ombrage_lisse.tga") ;
+}
+
 int main(int argc, char** argv){
 	readFile("obj/african_head.obj") ;
-	zbuffer = new int*[w] ;
+	zbuffer = new float*[w] ;
 	viewport(0,0,w,w) ;
 	lookat(cam, u, center) ;
-	project(0) ;
+	project(-1.f/(cam-center).norm()) ;
 	M = view*projection*changementBase ;
 	M_inv = (projection*changementBase).invert_transpose() ;
-	
+	lumiere = proj<3>(projection*changementBase*embed<4>(lumiere, 0.f)).normalize() ;
 	transform() ;
 	createImage() ;
 	return 0 ;
